b458dd8d5b72355940f2a0bc097dfd87c3ea4cce
jie guo <2623350233@qq.com>
2024-06-17 08:19:42 +0800

    feat: section-5finish

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 设置静态资源访问目录　但是要将上面的nestexpressapplication泛型传入才有这些方法
+  app.useStaticAssets('public', { prefix: '/staic' });
   await app.listen(3000);
 }
 bootstrap();
